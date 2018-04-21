3e5f57d58e09fb014938ee43ea5751342c1618c2
Profesor <micorreo@corr.es>
2018-04-21 18:38:28 +0200

    Cambios y comentarios javadoc

--- src/com/iesseveroochoa/Main.java
+++ src/com/iesseveroochoa/Main.java
@@ -4,14 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Valoración de " + args[0] + ": " + args[1] + " visto / exito " + args[2]);
-        System.out.println(valoracion(args[1],args[2]));
+        System.out.println(valoracion(args[1], args[2]));
     }
 
-    public static String valoracion(String valor1, String valor2){
+    /**
+     * Diagnostica el resultado obtenido.
+     * @param valor1 nota prevista
+     * @param valor2 nota conseguida
+     * @return mensaje personalizado.
+     */
+    private static String valoracion(String valor1, String valor2) {
         if (valor1.equals(valor2)) {
             return "Perfecto. Es lo previsto";
         }
-        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
             return "Has mejorado las espectativas";
         }
         return "Estas sobrevalorado";
